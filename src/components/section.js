7b6bf9dd9fa952b74ca74d1ed36608a2f8c949eb
Alan Campos <alancampos@me.com>
2018-11-23 18:24:05 -0500

    First commit

---
+++ src/components/section.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Section = ({ id, children }) => (
+  <div id={id} role="section">
+    {children}
+  </div>
+)
+
+Section.propTypes = {
+  children: PropTypes.node.isRequired,
+  id: PropTypes.string,
+}
+
+Section.defaultProps = {
+  id: '',
+}
+
+export default Section
