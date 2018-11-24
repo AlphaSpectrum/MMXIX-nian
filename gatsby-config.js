d71a3bebf8123b114dafc283c8ecb1741987c9f4
Alan Campos <alancampos@me.com>
2018-11-24 03:42:23 -0500

    Added favicon plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    keywords: 'portfolio',
-    description: 'Some description',
+    title: 'arc | Homepage',
+    keywords: 'javascript, gatsbyjs, web developer, engineer, software, software engineer, react, machine learning, ai, artificial intelligence, math, mathematics, webpack, html, css',
+    description: 'Software enginer and web developer Alan Campos',
     siteURL: 'https://www.alan-campos.com'
   },
   plugins: [
@@ -11,7 +11,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${ __dirname }/src/images`,
       },
     },
     'gatsby-transformer-sharp',
@@ -25,11 +25,44 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-     'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/images/logo.png',
+
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: true,
+          favicons: true,
+          firefox: true,
+          opengraph: true,
+          twitter: true,
+          yandex: true,
+          windows: true
+        }
+      }
+    },
   ],
 }
